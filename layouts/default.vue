ff82188d6a9b82c694273caf686154103aa5645b
watanabe_azusa <watanabe_azusa@mbpsoft.co.jp>
2021-02-20 14:17:55 +0900

    Refactoring by Prettier

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,22 @@
 <template>
-<v-app>
-  <div class="app">
-    <!-- コンテンツ -->
-    <v-content>
-      <router-view/>
-    </v-content>
-  </div>
-</v-app>
+  <v-app>
+    <div class="app">
+      <!-- Contents -->
+      <v-content>
+        <router-view />
+      </v-content>
+    </div>
+  </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
 
 <style>
-  /* リンクの下線を消す用 */
-  a {
-    text-decoration: none !important;
-  }
+a {
+  text-decoration: none !important;
+}
 </style>
