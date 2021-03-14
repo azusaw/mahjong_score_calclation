9cf945d19bc6f6ec3ca4e0b8dfdbe01239476971
azusaw <azusa0304@gmail.com>
2021-03-14 13:02:01 +0900

    Deleted not required file

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,13 +26,7 @@ export default {
       },
       { hid: "og:type", property: "og:type", content: "website" },
     ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      // {
-      //   rel: "stylesheet",
-      //   href: "https://fonts.googleapis.com/css?family=Noto+Sans+JP",
-      // },
-    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   /*
    ** Customize the progress-bar color
